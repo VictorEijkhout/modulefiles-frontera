e565be2307d7783a98a957bf56df29cbc5b5cfe0
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2025-01-17 13:26:04 -0600

    pkg config fix

--- Compiler/gcc/13.2.0/impi/21.9.0.lua
+++ Compiler/gcc/13.2.0/impi/21.9.0.lua
@@ -3,6 +3,7 @@ inherit()
 local mpi     = "impi"
 local version = "21.9.0"
 
+prepend_path( "PKG_CONFIG_PATH", pathJoin( "/opt/intel/oneapi/mpi", local_version, "lib/pkgconfig" ) )
 prepend_path("MODULEPATH",pathJoin(os.getenv("VICTOR_MODULEPATH_ROOT"),"MPI",os.getenv("TACC_FAMILY_COMPILER"),os.getenv("TACC_FAMILY_COMPILER_VERSION"),mpi,version))
 
 family("mpi")
